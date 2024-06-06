d67d63dea6f4eb59a734f84963ba667e272fb7e0
Abril <lopez.abrilagustina6@gmail.com>
2024-06-06 17:34:35 -0300

    Desarrollo class ProductoArchivo

--- Mostasa/ProductosArchivo.cpp
+++ Mostasa/ProductosArchivo.cpp
@@ -1,1 +1,131 @@
 #include "ProductosArchivo.h"
+#include <iostream>
+using namespace std;
+#include <string>
+#include <cstring>
+ProductoArchivo::ProductoArchivo(){
+    _Filename = "Productos.dat";
+}
+
+ProductoArchivo::ProductoArchivo(string Filename){
+     _Filename=Filename;
+}
+
+
+bool ProductoArchivo::abrirA(string modo){
+    _p = fopen(_Filename.c_str(), modo.c_str());
+    if(_p==nullptr){
+        cout<<"No se pudo abrir el archivo correctamente"<<endl;
+        return false;
+    }
+    else{return true;}
+}
+void ProductoArchivo::cerrarA(){
+    fclose(_p);
+
+}
+
+bool ProductoArchivo::guardar(Producto x){
+    if(abrirA("ab")==false){
+        cout<<"No se pudo abrir el archivo correctamente"<<endl;
+        return false;
+    }
+    bool grabo = fwrite(&x,sizeof(Producto),1,_p);
+    cerrarA();
+    return grabo;
+}
+
+int ProductoArchivo::buscar(int numE){ ///BUSCA PRODUCTO POR ID
+    Producto x;
+    int pos=0;
+     if(abrirA("rb")==false){
+        cout<<"No se pudo abrir el archivo correctamente"<<endl;
+        return -1;
+    }
+    while(fread(&x,sizeof(Producto),1,_p)){
+        if(x.getID()==numE){
+            break;
+        }
+        pos++;
+    }
+    cerrarA();
+    if(x.getID() == numE){
+        return pos;
+    }
+    else{return -1;}
+
+}
+Producto ProductoArchivo::leer(int pos){
+    Producto x;
+    if(abrirA("rb")==false){
+        cout<<"No se pudo abrir el archivo correctamente"<<endl;
+        x.setID(-1);
+        return x;
+    }
+    fseek(_p,sizeof (Producto)*pos,0);
+    fread(&x,sizeof(Producto),1,_p);
+    cerrarA();
+    return x;
+}
+int ProductoArchivo::getCantidadRegistros(){
+    if (abrirA("rb")==false){
+            cout<<"No se pudo abrir el archivo correctamente"<<endl;
+            return 0;
+    }
+    fseek(_p,0,2);
+    int cantbyte = ftell(_p);
+    int cantreg = cantbyte/sizeof(Producto);
+
+    cerrarA();
+    return cantreg;
+
+}
+
+bool ProductoArchivo::Modificar(Producto x,int pos){
+   bool guardado;
+   if (abrirA("rb+")==false){
+    cout<<"No se encontró el archivo."<<endl;
+    return false;
+   }
+   fseek(_p,sizeof(Producto)*pos,0);
+   guardado = fwrite (&x,sizeof(Producto),1,_p);
+   cerrarA();
+   return guardado;
+ }
+
+bool ProductoArchivo::realizarCopia(string filename){
+    FILE *pBak;
+    Producto x;
+
+    pBak=fopen(filename.c_str(),"wb");
+    if(pBak==nullptr){
+        return false;
+    }
+
+    int cantreg = getCantidadRegistros();
+    for(int i=0;i<cantreg;i++){
+        x = leer(i);
+        fwrite(&x,sizeof(Producto),1,pBak);
+    }
+    fclose(pBak);
+    return true;
+}
+
+bool ProductoArchivo::restaurarCopia(string filename){
+    Producto x;
+    ProductoArchivo archibak(filename.c_str());
+
+    if(!abrirA("wb")){
+        return false;
+    }
+
+    int cantreg = archibak.getCantidadRegistros();
+
+
+    for(int i=0;i<cantreg;i++){
+        x = archibak.leer(i);
+        fwrite(&x,sizeof(Producto),1,_p);
+    }
+    cerrarA();
+    return true;
+}
--- Mostasa/ProductosArchivo.h
+++ Mostasa/ProductosArchivo.h
@@ -1,1 +1,28 @@
 #pragma once
+#include <iostream>
+#include "Productos.h"
+class ProductoArchivo{
+    private:
+    bool abrirA(std::string modo = "rb");
+    void cerrarA();
+    FILE *_p;
+    std::string _Filename;
+public:
+    ProductoArchivo();
+    ProductoArchivo(std::string Filename);
+    bool guardar(Producto x);
+    int buscar(int numE);
+    Producto leer(int pos);
+    int getCantidadRegistros();
+    bool Modificar(Producto x,int pos);
+
+    bool realizarCopia(std::string filename = "Productos.bak");
+    bool restaurarCopia(std::string filename = "Productos.bak");
+
+    bool ListarOrdenado();
+    void CopiarRegVec(Producto v, int cant);
+    void ordenarporhabitantes(Producto v,int cant);
+
+
+
+};
